30f945cb9039e62e087cf58ec51d500258176d67
Sponge Guy <realevan03@gmail.com>
2024-12-14 16:01:30 -0500

    `BYTE`, `get_byte_instruction_data`, `get_length_of_statement`

--- main.py
+++ main.py
@@ -1,65 +1,65 @@
 import re
 
 OPCODE_TABLE = {
-	"ADD": [0x18, 3],
-	"ADDF": [0x58, 3],
-	"ADDR": [0x90, 2],
-	"AND": [0x40, 3],
-	"CLEAR": [0xB4, 2],
-	"COMP": [0x28, 3],
-	"COMPF": [0x88, 3],
-	"COMPR": [0xA0, 2],
-	"DIV": [0x24, 3],
-	"DIVF": [0x64, 3],
-	"DIVR": [0x9C, 2],
-	"FIX": [0xC4, 1],
-	"FLOAT": [0xC0, 1],
-	"HIO": [0xF4, 1],
-	"J": [0x3C, 3],
-	"JEQ": [0x30, 3],
-	"JGT": [0x34, 3],
-	"JLT": [0x38, 3],
-	"JSUB": [0x48, 3],
-	"LDA": [0x00, 3],
-	"LDB": [0x68, 3],
-	"LDCH": [0x50, 3],
-	"LDF": [0x70, 3],
-	"LDL": [0x08, 3],
-	"LDS": [0x6C, 3],
-	"LDT": [0x74, 3],
-	"LDX": [0x04, 3],
-	"LPS": [0xD0, 3],
-	"MUL": [0x20, 3],
-	"MULF": [0x60, 3],
-	"MULR": [0x98, 2],
-	"NORM": [0xC8, 1],
-	"OR": [0x44, 3],
-	"RD": [0xD8, 3],
-	"RMO": [0xAC, 2],
-	"RSUB": [0x4C, 3],
-	"SHIFTL": [0xA4, 2],
-	"SHIFTR": [0xA8, 2],
-	"SIO": [0xF0, 1],
-	"SSK": [0xEC, 3],
-	"STA": [0x0C, 3],
-	"STB": [0x78, 3],
-	"STCH": [0x54, 3],
-	"STF": [0x80, 3],
-	"STI": [0xD4, 3],
-	"STL": [0x14, 3],
-	"STS": [0x7C, 3],
-	"STSW": [0xE8, 3],
-	"STT": [0x84, 3],
-	"STX": [0x10, 3],
-	"SUB": [0x1C, 3],
-	"SUBF": [0x5C, 3],
-	"SUBR": [0x94, 2],
-	"SVC": [0xB0, 2],
-	"TD": [0xE0, 3],
-	"TIO": [0xF8, 1],
-	"TIX": [0x2C, 3],
-	"TIXR": [0xB8, 2],
-	"WD": [0xDC, 3],
+	"ADD": (0x18, 3),
+	"ADDF": (0x58, 3),
+	"ADDR": (0x90, 2),
+	"AND": (0x40, 3),
+	"CLEAR": (0xB4, 2),
+	"COMP": (0x28, 3),
+	"COMPF": (0x88, 3),
+	"COMPR": (0xA0, 2),
+	"DIV": (0x24, 3),
+	"DIVF": (0x64, 3),
+	"DIVR": (0x9C, 2),
+	"FIX": (0xC4, 1),
+	"FLOAT": (0xC0, 1),
+	"HIO": (0xF4, 1),
+	"J": (0x3C, 3),
+	"JEQ": (0x30, 3),
+	"JGT": (0x34, 3),
+	"JLT": (0x38, 3),
+	"JSUB": (0x48, 3),
+	"LDA": (0x00, 3),
+	"LDB": (0x68, 3),
+	"LDCH": (0x50, 3),
+	"LDF": (0x70, 3),
+	"LDL": (0x08, 3),
+	"LDS": (0x6C, 3),
+	"LDT": (0x74, 3),
+	"LDX": (0x04, 3),
+	"LPS": (0xD0, 3),
+	"MUL": (0x20, 3),
+	"MULF": (0x60, 3),
+	"MULR": (0x98, 2),
+	"NORM": (0xC8, 1),
+	"OR": (0x44, 3),
+	"RD": (0xD8, 3),
+	"RMO": (0xAC, 2),
+	"RSUB": (0x4C, 3),
+	"SHIFTL": (0xA4, 2),
+	"SHIFTR": (0xA8, 2),
+	"SIO": (0xF0, 1),
+	"SSK": (0xEC, 3),
+	"STA": (0x0C, 3),
+	"STB": (0x78, 3),
+	"STCH": (0x54, 3),
+	"STF": (0x80, 3),
+	"STI": (0xD4, 3),
+	"STL": (0x14, 3),
+	"STS": (0x7C, 3),
+	"STSW": (0xE8, 3),
+	"STT": (0x84, 3),
+	"STX": (0x10, 3),
+	"SUB": (0x1C, 3),
+	"SUBF": (0x5C, 3),
+	"SUBR": (0x94, 2),
+	"SVC": (0xB0, 2),
+	"TD": (0xE0, 3),
+	"TIO": (0xF8, 1),
+	"TIX": (0x2C, 3),
+	"TIXR": (0xB8, 2),
+	"WD": (0xDC, 3),
 }
 
 REG_VALUES = {
@@ -75,16 +75,30 @@ REG_VALUES = {
 }
 
 DIRECTIVES = {
+	"RESD",
+	"RESQ",
+	"NOBASE",
+	"EQU",
 	"BYTE",
 	"WORD",
 	"RESW",
 	"RESB",
+	"BASE",
 }
 
 SYMBOL_TABLE = []
 
 PROGRAM_COUNTER = 0x0000
-BASE_REGISTER = 0x0000
+
+REG_A = 0
+REG_X = 0
+REG_L = 0
+REG_B = 0
+REG_S = 0
+REG_T = 0
+REG_F = 0
+
+sample = "samples/functions.txt"
 
 def bin_to_hex(value):
 	decimal = int(value, 2)
@@ -98,23 +112,42 @@ def hex_to_bin(value):
 	padded_binary = binary.zfill(len(value) * 4)
 	return padded_binary
 
+def get_length_of_statement(statement):
+	directive_lengths = {
+		"RESD": lambda x: 4 * int(x[2]),
+		"RESQ": lambda x: 8 * int(x[2]),
+		"RESW": lambda x: 3 * int(x[2]),
+		"RESB": lambda x: int(x[2]),
+		"BYTE": lambda x: get_byte_instruction_data(x)[1],
+		"WORD": lambda x: 3,
+		"EQU": lambda x: 0 if operand.startswith('*') else None,
+		"BASE": lambda x: 0,
+		"NOBASE": lambda x: 0,
+	}
+	try:
+		instruction = statement[1]
+		operand = statement[2]
+		function = directive_lengths.get(instruction)
+		if function:
+			return int(function(statement))
+
+		# if not return here, then instruction is an operation
+		if parse_instruction(instruction) in OPCODE_TABLE:
+			if '+' in instruction:
+				return 4
+			else:
+				return OPCODE_TABLE[instruction][1]
+	except Exception as e:
+		print(f"Could not get length of instruction: {e}")
+
 # take a parsed_instruction (no special characters) and returns the next pc variable based on the size of instruction
-def get_next_PC(instruction):
+def get_next_PC(statement):
 	global PROGRAM_COUNTER
 	try:
-		parsed_instruction = parse_instruction(instruction)
 		c = PROGRAM_COUNTER
-		#print("ughhhh", parsed_instruction)
-		if parsed_instruction in OPCODE_TABLE or parsed_instruction in DIRECTIVES:
-			#print("yes")
-			if '+' in instruction:
-				c += 0x4
-				#print("4")
-			elif parsed_instruction in OPCODE_TABLE:
-				c += OPCODE_TABLE[parsed_instruction][1]
-				#print("3")
-			else:
-				c += 3
+		value = get_length_of_statement(statement)
+		if value:
+			c += value
 		return c
 	except Exception as e:
 		print(f"An error occurred while getting next PC: {e}")
@@ -137,8 +170,26 @@ def parse_instruction(instruction):
 	except Exception as e:
 		print(f"Error while parsing instruction: {e}")
 
+def get_byte_instruction_data(statement):
+	# given the instruction is BYTE, get the length of the instruction and the object code
+	try:
+		value = 0
+		match = re.findall(r"^([CX])'([A-Za-z0-9]+)'$", statement[2])
+		if match[0][0] == 'X':
+			# operand is hexadecimal constant
+			value = match[0][1]
+		elif match[0][0] == 'C':
+			# operand is a string of characters
+			value = ''.join([format(ord(c), '02X') for c in match[0][1]])
+
+		length = len(value) / 2
+		obj_code = "0x" + value
+		return (obj_code, length)
+	except Exception as e:
+		print(f"error getting BYTE instruction data: {e}")
+
 def first_pass():
-	path = "samples/functions.txt"
+	path = sample
 	global PROGRAM_COUNTER
 	# iterate through file
 	try:
@@ -159,9 +210,12 @@ def first_pass():
 				
 				if statement[0] != '':
 					SYMBOL_TABLE.append([hex(PROGRAM_COUNTER), statement[0]])
-				
-				if len(statement) >= 2 and statement[1]:
-					PROGRAM_COUNTER = get_next_PC(statement[1])
+
+				if statement[0] == '.' and re.search(r'^[A-Fa-f0-9]+$', statement[1]):
+					pass
+					#PROGRAM_COUNTER = int(statement[1], 16)
+				elif len(statement) >= 2 and statement[1]:
+					PROGRAM_COUNTER = get_next_PC(statement)
 				
 				
 		print(SYMBOL_TABLE)
@@ -169,12 +223,12 @@ def first_pass():
 	except FileNotFoundError:
 		print(f"File not found: {path}")
 	except Exception as e:
-		print(f"An error occurred while first passing: {e}")
+		print(f"An error occurred during first pass: {e}")
 
 
 
 def second_pass():
-	path = "samples/functions.txt"
+	path = sample
 	global PROGRAM_COUNTER
 	# iterate through file
 	try:
@@ -183,9 +237,16 @@ def second_pass():
 			for line in file:
 				statement = line.split('\t')
 				print(statement)
-				get_addressing_mode(statement)
-				if len(statement) >= 2 and statement[1]:
-					PROGRAM_COUNTER = get_next_PC(statement[1])
+
+				if len(statement) >= 2 and statement[1] and statement[1] == 'BASE':
+					set_base_register(statement)
+
+				generate_obj_code(statement)
+				if statement[0] == '.' and re.search(r'^[A-Fa-f0-9]+$', statement[1]):
+					pass
+					#PROGRAM_COUNTER = int(statement[1], 16)
+				elif len(statement) >= 2 and statement[1]:
+					PROGRAM_COUNTER = get_next_PC(statement)
 				
 		print(SYMBOL_TABLE)
 
@@ -194,6 +255,16 @@ def second_pass():
 	except Exception as e:
 		print(f"An error occurred: {e}")
 	pass
+
+def set_base_register(statement):
+	global REG_B
+	operand = statement[2]
+	try:
+		for entry in SYMBOL_TABLE:
+			if operand == entry[1]:
+				REG_B = int(entry[0][2:], 16)
+	except Exception as e:
+		print(f"an error occurred while setting base register: {e}")
 
 def parse_operand(operand):
 	try:
@@ -205,7 +276,7 @@ def parse_operand(operand):
 def wrap_4bit_hex(negative_hex):
     return str(hex(0xffff + (negative_hex + 0x1)))
 
-def get_addressing_mode(statement):
+def generate_obj_code(statement):
 	nixbpe = [0, 0, 0, 0, 0, 0]
 	instruction = None
 	operand = None
@@ -280,6 +351,7 @@ def get_addressing_mode(statement):
 		# get address of operand
 		for entry in SYMBOL_TABLE:
 			if operand == entry[1]:
+				print(type(entry[0]), entry[0])
 				m = bin(int(entry[0][2:], 16))[2:].zfill(8)
 
 	# get obj code as a string of binary
@@ -306,6 +378,11 @@ def get_addressing_mode(statement):
 
 	obj_code = None
 
+	# instruction is a directive
+	if form == 0:
+		if statement[1] == 'BYTE':
+			obj_code, code_length = get_byte_instruction_data(statement)
+
 	if form == 2:
 		try:
 			# in format 2, registers are 1-byte values represented in object code
@@ -329,6 +406,7 @@ def get_addressing_mode(statement):
 		try:
 			if m:
 				# assume pc relative unless disp does not follow rules
+				# operand is not a constant, so we are doing relative addresing or extended format
 				if nixbpe[5] == 0: # if no extended format, then relative addressing.
 					nixbpe[4] = 1
 
@@ -358,25 +436,46 @@ def get_addressing_mode(statement):
 			# pc-relative, calculate disp here
 			if nixbpe[4] == 1:
 				print("assuming pc-relative")
-				PC = get_next_PC(instruction)
-				print(int(m, 2) - PC)
-				disp = int(m, 2) - PC
+				PC = get_next_PC(statement)
 
-				if disp < -2048:
-					raise Exception("ERROR: displacement value cannot be less than 2048")
-				if disp > 2047:
-					raise Exception("base relative handle here")
+				disp = int(m, 2) - PC
+				print(f"disp: {disp}")
+
+				if disp < -2048 or disp > 2047:
+					# pc-relative displacement value out of range, try base relative
+					print("pc-relative displacement out of range")
+					nixbpe[4] = 0
+					nixbpe[3] = 1
 
 				if disp < 0:
 					disp = bin(int(wrap_4bit_hex(disp)[2:], 16))[2:].zfill(16)
-					print(disp, type(disp))
 				else:
 					disp = bin(disp)[2:].zfill(16)
 
 				disp = disp[4:]
 
+
+			# if pc-relative fails, then base relative protocol will activate
+			if nixbpe[3] == 1:
+				print("using base relative")
+				print(f"REG_B: {REG_B}")
+				# dont forget m is target address
+				disp = int(m, 2) - REG_B
+				print(f"disp: {disp}")
+
+				if disp < 0 or disp > 4095:
+					raise Exception("both pc-relative and base-relative displacement values are out of bounds")
+
+				disp = bin(disp)[2:].zfill(16)
+				disp = disp[4:]
+
+			# subtract index register value if applicable
 			if nixbpe[2] == 1:
-				raise Exception("i dont know how to do index addressing yet")
+				print(f"REG_X: {REG_X}")
+				disp = int(disp, 2) - REG_X
+				disp = bin(disp)[2:].zfill(16)
+				disp = disp[4:]
+
 		except Exception as e:
 			print(f"error calculating object code: {e}")
 
